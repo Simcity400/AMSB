939fcacee1f0cbeddc9f5ed24fa84a04ea8ab344
Anomaky <kecapexo@gmail.com>
2025-04-13 12:38:01 +1000

    S1

---
+++ common/ideas/_no_not_right_now = {.lua
@@ -0,0 +1,89 @@
+_no_not_right_now = {
+		
+			allowed = {
+				original_tag = 
+				always = no
+			}
+
+			allowed_civil_war = {
+				always = yes
+			}			
+			
+			available = {
+			}
+
+			picture = generic_infantry_bonus
+			
+			removal_cost = -1
+
+			equipment_bonus = {
+equipment_bonus = {
+    # Aircraft
+    fighter_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    cv_fighter_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    heavy_fighter_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    strategic_bomber_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    tac_bomber_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    cas_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    nav_bomber_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    cv_nav_bomber_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    cv_cas_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    scout_plane_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    jet_fighter_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    jet_tac_bomber_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+    jet_strat_bomber_equipment = {
+        build_cost_ic = 1000 instant = yes
+    }
+
+    # Naval
+    submarine = {
+        build_cost_ic = 1000 instant = yes
+    }
+    destroyer = {
+        build_cost_ic = 1000 instant = yes
+    }
+    light_cruiser = {
+        build_cost_ic = 1000 instant = yes
+    }
+    heavy_cruiser = {
+        build_cost_ic = 1000 instant = yes
+    }
+    battle_cruiser = {
+        build_cost_ic = 1000 instant = yes
+    }
+    battleship = {
+        build_cost_ic = 1000 instant = yes
+    }
+    carrier = {
+        build_cost_ic = 1000 instant = yes
+    }
+    convoy = {
+        build_cost_ic = 1000 instant = yes
+    }
+}
+			}
+		}
