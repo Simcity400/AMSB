a5d1dd6f1f3f079fe54448dfba10583c9198e195
Anomaky <83631382+Anomaky@users.noreply.github.com>
2025-05-07 21:56:40 +1000

    Air rework defines (I'll find the air efficiency flickering changes)
    -- Higher value = more shot down planes
    -- How many planes can shoot at each plane on other side ( if there are 100 planes we are atttacking COMBAT_MULTIPLANE_CAP * 100 of our planes can shoot )

--- common/defines/02_huski_defines.lua
+++ common/defines/02_huski_defines.lua
@@ -1,2 +1,11 @@
 NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.25
 NDefines.NMilitary.PEN_VS_AVERAGE = 0.3
+
+NDefines.NGeography.MEDITERRANEAN_SEA_REGIONS = { 15, 29, 68, 69, 168, 169, 202, 299, 300, 301, } -- The sea regions that are considered as part of the Mediterranean sea
+
+-- Air Rework
+NDefines.NMilitary.LAND_AIR_COMBAT_MAX_PLANES_PER_ENEMY_WIDTH = 1   --vanilla 3
+NDefines.NAir.DISRUPTION_FACTOR = 5 --cas enters battle at 30+%
+NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 0.0
+NDefines.NAir.COMBAT_MULTIPLANE_CAP = 3							-- How many planes can shoot at each plane on other side ( if there are 100 planes we are atttacking COMBAT_MULTIPLANE_CAP * 100 of our planes can shoot )
+NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.7						-- Higher value = more shot down planes
