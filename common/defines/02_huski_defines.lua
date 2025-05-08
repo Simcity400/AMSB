0956aa67336c5bf53b9e3cf9c1335bb530340ab0
Simcity400 <64632025+Simcity400@users.noreply.github.com>
2025-05-08 19:50:56 +1000

    Rubber reintroduced into self sealing, Germany rubber output increased from focus, air defines reverted for now until we decide on air rework

--- common/defines/02_huski_defines.lua
+++ common/defines/02_huski_defines.lua
@@ -1,14 +1,2 @@
 NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.25
 NDefines.NMilitary.PEN_VS_AVERAGE = 0.3
-
-NDefines.NGeography.MEDITERRANEAN_SEA_REGIONS = { 15, 29, 68, 69, 168, 169, 202, 299, 300, 301, } -- The sea regions that are considered as part of the Mediterranean sea
-
- ---AS KAR MOD 
- NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_DEFAULT = 0.25	-- Default how much efficiency to regain per day. Gain applied hourly.
- NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_STRATEGIC_BOMBER = 0.06	-- How much efficiency to regain per day. Gain applied hourly.
- NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_CAS = 1.5				-- How much efficiency to regain per day. Gain applied hourly.
- NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_NAVAL_BOMBER = 1.4	-- How much efficiency to regain per day. Gain applied hourly.
- NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_TACTICAL_BOMBER = 0.06	-- How much efficiency to regain per day. Gain applied hourly.
- NDefines.NAir.EFFICIENCY_REGION_CHANGE_DAILY_GAIN_FIGHTER = 1.5			-- How much efficiency to regain per day. Gain applied hourly.
- 
- NDefines.NAir.EFFICIENCY_REGION_CHANGE_PENALTY_FACTOR = 1.0				-- Penalty applied for changing region
