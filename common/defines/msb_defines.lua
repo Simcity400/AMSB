69f21f515f1d2e440f7ca36a3f0fd708313229a6
Simcity4000 <100258256+Simcity4000@users.noreply.github.com>
2025-05-26 15:17:57 +1000

    Max factories per production line increased to 200 (from 150)

--- common/defines/msb_defines.lua
+++ common/defines/msb_defines.lua
@@ -239,6 +239,7 @@ NDefines.NProduction.FLOATING_HARBOR_MAX_NAV_FACTORIES_PER_LINE = 50
 NDefines.NProduction.CONVOY_MAX_NAV_FACTORIES_PER_LINE = 150 
 NDefines.NProduction.CAPITAL_SHIP_MAX_NAV_FACTORIES_PER_LINE = 10
 NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 4.50
+NDefines.NProduction.MAX_MIL_FACTORIES_PER_LINE = 200
 NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 0.0
 NDefines.NMilitary.LAND_AIR_COMBAT_MAX_PLANES_PER_ENEMY_WIDTH = 1
 NDefines.NGeography.MEDITERRANEAN_SEA_REGIONS = { 15, 29, 68, 69, 168, 169, 202, 299, 300, 301, } -- The sea regions that are considered as part of the Mediterranean sea
