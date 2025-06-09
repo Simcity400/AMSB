8e8f0f8a47ea24a2856ce4ff215565051a73c1b3
Simcity400 <64632025+Simcity400@users.noreply.github.com>
2025-06-09 18:01:20 +1000

    Convoy bug fix

--- common/defines/msb_defines.lua
+++ common/defines/msb_defines.lua
@@ -244,4 +244,4 @@ NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 0.0
 NDefines.NMilitary.LAND_AIR_COMBAT_MAX_PLANES_PER_ENEMY_WIDTH = 1
 NDefines.NGeography.MEDITERRANEAN_SEA_REGIONS = { 15, 29, 68, 69, 168, 169, 202, 299, 300, 301, } -- The sea regions that are considered as part of the Mediterranean sea
 NDefines.NIndustrialOrganisation.DESIGN_TEAM_RESEARCH_BONUS = 0.1
-NDefines.NGraphics.CONVOY_LENDLEASE_RANGE_FACTOR = 0.1
+NDefines.NCountry.CONVOY_LENDLEASE_RANGE_FACTOR = 0.01
