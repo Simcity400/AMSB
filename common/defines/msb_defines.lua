3a65688c71af43ae88d912eb8a19adbdb5751a06
Simcity400 <powntastic@gmail.com>
2026-02-08 14:40:51 +1100

    Rebalanced AA effectiveness

--- common/defines/msb_defines.lua
+++ common/defines/msb_defines.lua
@@ -271,7 +271,8 @@ NDefines.NAir.DISRUPTION_FACTOR = 5.5 --cas enters battle at 30+%
 NDefines.NAir.AIR_MORE_GROUND_CREWS_COST = 0.0
 NDefines.NAir.COMBAT_MULTIPLANE_CAP = 3					-- How many planes can shoot at each plane on other side ( if there are 100 planes we are atttacking COMBAT_MULTIPLANE_CAP * 100 of our planes can shoot )
 NDefines.NAir.COMBAT_DAMAGE_SCALE = 0.45				-- Higher value = more shot down planes
-NDefines.NAir.ANTI_AIR_PLANE_DAMAGE_CHANCE = 0.033
+NDefines.NAir.ANTI_AIR_PLANE_DAMAGE_CHANCE = 0.08
+NDefines.NAir.ANTI_AIR_PLANE_DAMAGE_FACTOR = 0.5					-- Anti Air Gun Damage factor
 -- Bombing Fix
 NDefines.NSupply.RAILWAY_FLOW_PENALTY_PER_DAMAGED = 4.9
 NDefines.NAir.AIR_WING_BOMB_DAMAGE_FACTOR = 0.6 --Chrom Bombing Fix; HMM = 0.6
